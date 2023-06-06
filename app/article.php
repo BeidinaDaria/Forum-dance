7dcb5d3a25f95948377c5b3739064f172db5a9b7
Harley <92978645+BeidinaDaria@users.noreply.github.com>
2023-06-06 21:03:09 +0300

    Update article.php

--- app/article.php
+++ app/article.php
@@ -3,9 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
 
 class article extends Model
 {
-    protected $table="articles";
-    protected $fillable=['title'];
+    protected $fillable=[
+        'title',
+        'text',
+        'image',
+        'date of publishing'
+    ];
+    public function getSlugOptions():SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+    public static function add(array $input){
+        $article=new static;
+        $article->fill($input);
+        $article->save();
+        return $article;
+    }
+    public function getPublishStatus(){
+        if ($this->is_published){
+            return '<p style="color:red;">Yes</p>';
+        }
+        return '<p style="color:red;">No</p>';
+    }
 }
