1081b0670df31c2349f8ea109e972918631140f0
Harley <92978645+BeidinaDaria@users.noreply.github.com>
2023-06-16 15:28:50 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,12 +21,21 @@ Route::get('/',[AppController::class,"mainPage"])->name("app.main");
 //Articles
 Route::prefix('articles')->group(function(){
     Route::get('/',[ArticleController::class,'index'])->name('articles.list');
-    Route::get('create',[ArticleController::class,'createArticle'])->name('articles.create');
-    Route::post('create',[ArticleController::class,'storeArticle'])->name('articles.store');
-    Route::get('{articleId}/edit',[ArticleController::class,'editArticle'])->name('articles.edit');
-    Route::put('{articleId}/edit',[ArticleController::class,'updateArticle'])->name('articles.edit');
-    Route::delete('{articleId}',[ArticleController::class,'deleteArticle'])->name('articles.delete');
-    Route::get('{articleSlug}',[ArticleController::class,'show'])->name('articles.show');
-
+    Route::middleware(['auth','is_admin'])->group(function(){
+        Route::get('create',[ArticleController::class,'createArticle'])->middleware(['auth','is_admin'])->name('articles.create');
+        Route::post('create',[ArticleController::class,'storeArticle'])->middleware(['auth','is_admin'])->name('articles.store');
+        Route::get('{articleId}/edit',[ArticleController::class,'editArticle'])->middleware(['auth','is_admin'])->name('articles.edit');
+        Route::put('{articleId}/edit',[ArticleController::class,'updateArticle'])->middleware(['auth','is_admin'])->name('articles.edit');
+        Route::delete('{articleId}',[ArticleController::class,'deleteArticle'])->middleware(['auth','is_admin'])->name('articles.delete');
+        Route::get('{articleSlug}',[ArticleController::class,'show'])->middleware(['auth','is_admin'])->name('articles.show');
+        Route::get('{articleId}/remove-image',[ArticleController::class,'removeImage'])->middleware(['auth','is_admin'])->name("articles.remove-image");
+    });
+    Route::post('logout',[AuthControler::class,'logout'])->middleware('guest')->name('auth.logout');
 });
 
+Route::middleware(['guest'])->group(function(){
+    Route::get('register',[AuthControler::class,'registerPage'])->name('auth.register');
+    Route::post('register',[AuthControler::class,'storeUser'])->name('auth.storeUser');
+    Route::get('login',[AuthControler::class,'loginPage'])->name('auth.login-page');
+    Route::post('login',[AuthControler::class,'login'])->name('auth.login');
+});
